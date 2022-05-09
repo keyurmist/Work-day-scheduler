bf77da4cd384aae0690c614518d3974ae90e1daa
KeyurMistry <keyurmist@gmail.com>
2022-05-09 22:16:14 +0100

    added color changes for past, present, future

--- Assets/script.js
+++ Assets/script.js
@@ -5,3 +5,26 @@ var saveBtn = $(".saveBtn");
 //Function for the current day//
 
 $("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
+
+//Function for changing colour whether time is past, present or future//
+
+function timeBlockColor() {
+
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+
+        var currentHour = parseInt($(this).attr("id"));
+
+        if (currentHour > hour) {
+            $(this).addclass(".future");
+
+        } else if (currentHour === hour) {
+            $(this).addClass(".present");
+            
+        } else {$(this).addClass(".past");
+    }
+    })
+};
+
+

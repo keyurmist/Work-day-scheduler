7ec3edd568f994d02cf5feabe4656977f8e386a9
KeyurMistry <keyurmist@gmail.com>
2022-05-09 22:47:06 +0100

    debugged js to get items to save upon refresh

--- Assets/script.js
+++ Assets/script.js
@@ -50,7 +50,7 @@ function planner() {
 
         if (currentPlan !==null) {
 
-            $(this).siblings(".plan").val(CurrentPlan);
+            $(this).siblings(".plan").val(currentPlan);
         }
 
     });
